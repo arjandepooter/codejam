5dbfe5c89afef0c3a9257870aff19e02701b8a69
arjan <arjansdijkstra@gmail.com>
2015-03-19 15:23:19 +0100

    Added solution for TSP

---
+++ 2014/round_1b/the_bored_traveling_salesman/solution.py
@@ -0,0 +1,81 @@
+'''
+graph = { "a" : ["c"],
+          "b" : ["c", "e"],
+          "c" : ["a", "b", "d", "e"],
+          "d" : ["c"],
+          "e" : ["c", "b"],
+          "f" : []
+        }
+'''
+import copy
+
+def is_connected(graph):
+    if graph=={}:
+        return True
+    seen = set([next(graph.iterkeys())])
+    tocheck = seen.copy()
+    while tocheck:
+        node = tocheck.pop()
+        tocheck = tocheck.union(set(graph[node]).difference(seen))
+        seen = seen.union(graph[node])
+    return len(seen)==len(graph)
+
+def deletenodes(graph, nodes):
+    for node in nodes:
+        del graph[node]
+    for i in graph.keys():
+        for node in nodes:
+            try:
+                graph[i].remove(node)
+            except:
+                pass
+    return graph
+
+def bestroute(graph):
+    tovisit = graph.keys()
+    visited = [min(tovisit)]
+    neighbors = set(graph[visited[0]])
+    returnflight = list(visited)
+    while len(visited)<len(tovisit):
+        candidates = sorted(neighbors, reverse=True)
+        candidateadded = False
+        while not candidateadded:
+            candidate = candidates.pop()
+            testgraph = copy.deepcopy(graph)
+            removedvertices = []
+            removedvertices.append(returnflight.pop())
+            while not candidate in graph[removedvertices[-1]]:
+                removedvertices.append(returnflight.pop())
+            testgraph =deletenodes(testgraph, removedvertices[:-1])
+            if is_connected(testgraph):
+                graph=copy.deepcopy(testgraph)
+                candidateadded=True
+                visited.append(candidate)
+                returnflight.append(removedvertices[-1])
+                returnflight.append(candidate)
+                neighbors=set()
+                for node in returnflight:
+                    neighbors = neighbors.union(set(graph[node]))
+                    neighbors = neighbors.difference(visited)
+            else:
+                returnflight += reversed(removedvertices)
+    return ''.join(visited)
+
+def get_output(instance):
+    inputdata = open(instance + ".in", 'r')
+    output = open(instance+ ".out", 'w')
+    T = int(inputdata.readline())
+    for t in range(T):
+        n,m = [int(i) for i in inputdata.readline().split()]
+        graph = {}
+        nodes = []
+        for i in range(n):
+            nodes.append(inputdata.readline()[:-1])
+            graph[nodes[i]]=[]
+        for i in range(m):
+            edge = [int(i)-1 for i in inputdata.readline().split()]
+            graph[nodes[edge[0]]].append(nodes[edge[1]])
+            graph[nodes[edge[1]]].append(nodes[edge[0]])
+        print t
+        output.write('Case #' + str(t+1) +': ' + bestroute(graph) +  "\n")
+    return None
