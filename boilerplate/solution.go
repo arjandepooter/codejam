c6947488e3bdb103ef05c43c7d4ad4e77ceb5ac4
Arjan de Pooter <arjan@cmsworks.nl>
2015-03-20 10:32:20 +0100

    Added Go boilerplate

---
+++ boilerplate/solution.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/arjandepooter/codejam/utils"
+)
+
+func main() {
+	input, err := utils.GetInput()
+	if err != nil {
+		fmt.Errorf("Error occured: %x", err)
+	}
+
+	problems := getProblems(input)
+
+	for i, problem := range problems {
+		fmt.Printf("Case #%d: %s\n", i+1, problem.Solve())
+	}
+}
+
+type Problem struct {
+}
+
+func (problem *Problem) Solve() string {
+	return "foo"
+}
+
+func getProblems(input *utils.Input) []*Problem {
+	n := input.ReadInt()
+
+	problems := make([]*Problem, n)
+	for i := 0; i < n; i++ {
+		problem := Problem{}
+
+		// Fill problem
+
+		problems[i] = &problem
+	}
+
+	return problems
+}
